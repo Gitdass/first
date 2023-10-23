6fdcd411d2020e95caed06fe40e27317e0fa8df4
Gitdass <gkdass6847@gmail.com>
2023-10-23 21:44:34 +0530

    hjj

--- Application1/Eight.cs
+++ Application1/Eight.cs
@@ -15,6 +15,7 @@ namespace Application1
 
             double kelvin = celsius + 273.15;
             double fahrenheit = (celsius * 9 / 5) + 32;
+
             Console.WriteLine("Expected Output:");
             Console.WriteLine($"Kelvin = {kelvin:F0}");
             Console.WriteLine($"Fahrenheit = {fahrenheit:F0}");
